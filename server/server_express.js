a584eda3d09528f5e963d5d8a53eedf2c540d03a
Ewan-James-Donovan <ewan.james.donovan@gmail.com>
2020-02-28 11:25:23 +0000

    Initial express server js

--- server/server_express.js
+++ server/server_express.js
@@ -1,1 +1,27 @@
-// This is where the express server is started.
+const app = require('express')();
+const db = require("./connect_db");
+const login_controller = require("../routes/login_controller");
+const { appURL } = require("../consts.json");
+const port = 8080;
+
+app.use(require("express").json());
+
+app.use((req, res, next) => {
+    const logEntry =
+        `
+    host: ${req.hostname}
+    ip: ${req.ip}
+    method: ${req.method}
+    path: ${req.path}
+    time: ${new Date()}`;
+    console.log(logEntry);
+    next();
+});
+
+app.use(appURL+ "/login", login_controller);
+
+const server = app.listen(port, () => {
+    console.log(`Server started successfully on port ${port}`);
+});
+
+module.exports = server;
